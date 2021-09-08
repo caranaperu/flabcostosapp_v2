e8096e0d07beade307dc33a8f66f1d9fdaa66f71
carlos <aranape@gmail.com>
2021-09-08 00:15:23 -0500

    2da Limpieza adicional de codigo no relevantre para este proyecto

--- application/backend/views/mainEntryView.php
+++
@@ -1,6 +0,0 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
-<html>
-<head>
-<title>Future Labs - Costos</title>
-<meta http-equiv="REFRESH" content="0;url=application/client/index.php"></HEAD>
-</html>
